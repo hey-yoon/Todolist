7d9739168b51fa9e09a6eab6ef3c0d29b585261d
hey-yoon <yooniyunisss@gmail.com>
2024-12-08 21:24:17 +0900

    AdminLayout 파일 구현

--- src/pages/layout/AdminLayout.js
+++ src/pages/layout/AdminLayout.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link, NavLink, Outlet } from 'react-router-dom';
+import S from './style';
 
+// 로그인 되었을 때의 layout
 const AdminLayout = () => {
     return (
-        <div>
-            
-        </div>
+        <S.Background>
+            <S.Wrapper>
+                <S.Header>
+                    <Link to={"/"}>Hyeyoon Todo</Link>
+                </S.Header>
+                
+                <main>
+                    <Outlet />
+                </main>
+
+                <S.Nav>
+                    <NavLink to={"/todo"}>
+                        <p>피드</p>
+                    </NavLink>
+                    <NavLink to={"/search"}>
+                        <p>검색</p>
+                    </NavLink>
+                    <NavLink to={"/notice"}>
+                        <p>알림</p>
+                    </NavLink>
+                    <NavLink to={"/my"}>
+                        <p>My</p>
+                    </NavLink>
+                </S.Nav>
+            </S.Wrapper>
+        </S.Background>
     );
 };
 
