1cc8e10c72009c91ffe60536baf0e720fd97790d
hey-yoon <yooniyunisss@gmail.com>
2024-12-18 23:02:30 +0900

    회원가입 페이지 수정

--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Form } from 'react-router-dom';
+import BasicInput from '../../components/input/BasicInput';
+import BasicButton from '../../components/button/BasicButton';
 
 const SignUp = () => {
     return (
-        <div>
-            
-        </div>
+        <form action="">
+            {/* 이메일 */}
+            <label>
+                <p>이메일</p>
+                <BasicInput size={"full"} shape={"small"} variant={"blue"} color={"black"} type="text" id="email" name="email" placeholder="아이디를 입력하세요."/>
+            </label>
+            {/* 비밀번호 */}
+            <label>
+                <p>비밀번호</p>
+                <BasicInput size={"full"} shape={"small"} variant={"blue"} color={"black"} type="text" id="password" name="password" placeholder="비밀번호를 입력하세요."/>
+            </label>
+            {/* 비밀번호 확인 */}
+            <label>
+                <p>비밀번호 확인</p>
+                <BasicInput size={"full"} shape={"small"} variant={"blue"} color={"black"} type="text" placeholder="비밀번호를 입력하세요."/>
+            </label>
+            <BasicButton size={"full"} shape={"small"} variant={"black"} color={"white"}>회원가입</BasicButton> 
+        </form>
     );
 };
 
