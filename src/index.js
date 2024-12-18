3d6a8a8018eb07c47679eea7bcda1c7ac1f5adeb
hey-yoon <yooniyunisss@gmail.com>
2024-12-19 01:50:27 +0900

    리덕스 추가

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 리덕스 코드 추가
+import {Provider} from 'react-redux';
+import {devToolsEnhancer} from '@redux-devtools/extension';
+import { legacy_createStore as createStore } from 'redux';
+import rootReducer from './modules';
+const store = createStore(rootReducer,devToolsEnhancer());
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>
 );
 
--- src/pages/error/PageNotFound.jsx
+++ src/pages/error/PageNotFound.jsx
@@ -8,7 +8,7 @@ const PageNotFound = () => {
     return (
         <div>
             <h1>원하시는 페이지를 찾을 수 없습니다.</h1>
-            {navigation('/main')}
+            {navigation('/')}
 
         </div>
     );
