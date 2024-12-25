6d413127b31d3dab997bbbca1aa5c854b824de31
hey-yoon <yooniyunisss@gmail.com>
2024-12-25 11:57:05 +0900

    비밀번호 부분

--- src/pages/signUp/SignUp.js
+++ src/pages/signUp/SignUp.js
@@ -53,7 +53,21 @@ const SignUp = () => {
             {/* 비밀번호 */}
             <label>
                 <p>비밀번호</p>
-                <BasicInput size={"full"} shape={"small"} variant={"blue"} color={"black"} type="text" id="password" name="password" placeholder="비밀번호를 입력하세요."/>
+                <BasicInput size={"full"} shape={"small"} variant={"blue"} color={"black"} type="text" id="password" name="password" placeholder="비밀번호를 입력하세요."
+                
+                {...register("password",{
+                    required: true,
+                    pattern:{
+                        value: passwordRegex,
+                    }
+                })}
+                />
+                {errors?.password?.type === "required" && (
+                    <p>비밀번호를 입력해주세요.</p>)
+                }
+                {errors?.password?.type === "pattern" && (
+                    <p>소문자, 숫자, 특수문자 각 하나씩 포함한 8자리 이상이여야 합니다. *!@#만 사용가능</p>)
+                }
             </label>
             {/* 비밀번호 확인 */}
             <label>
